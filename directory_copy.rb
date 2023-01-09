d6e7c69d3ea9a1fd85cbd41f910ec4fa21cbbeba
Ryan Lai <ryanlai12@gmail.com>
2023-01-09 13:47:24 +0000

    Added feature to allow user to add cohort month (program auto-capitalizes and converts the string to a symbol)

--- directory_copy.rb
+++ directory_copy.rb
@@ -4,7 +4,7 @@ def print_header
   puts "-------------".center(40)
 end
 
-def print(students, students_with_r, students_less_than_12)
+def print_students(students, students_with_r, students_less_than_12)
   
   puts "\nAll students:"
   students_printer(students)
@@ -17,7 +17,7 @@ end
 def students_printer(list_of_students)
   i = 0
   while i < list_of_students.length 
-    puts "#{i+1}. #{list_of_students[i][:name]}, who likes #{list_of_students[i][:hobby]} and was born in #{list_of_students[i][:cob]}."
+    puts "#{i+1}. #{list_of_students[i][:name]}, who likes #{list_of_students[i][:hobby]} and was born in #{list_of_students[i][:cob]} (#{list_of_students[i][:cohort]} cohort)."
     i += 1
   end
 end
@@ -40,6 +40,8 @@ def input_students()
   while !name.empty? do
   # Storing the cohort name as symbols because we don't expect to do 'String'-like activities
     name = name.downcase.capitalize()
+    puts "Please enter #{name}'s cohort month:"
+    cohort = input_info("cohort month").capitalize.to_sym
     puts "Please enter #{name}'s favorite hobby:"
     hobby = input_info("hobby")
     puts "Please enter #{name}'s country of birth:"
@@ -47,13 +49,13 @@ def input_students()
     cob = cob.downcase.capitalize()
 
     if name.split("")[0] == 'R'
-      students_with_r.push({name: name, cohort: :november, hobby: hobby, cob: cob})
+      students_with_r.push({name: name, cohort: cohort, hobby: hobby, cob: cob})
     end
     if name.split('').length < 12
-      students_less_than_12.push({name: name, cohort: :november, hobby: hobby, cob: cob})
+      students_less_than_12.push({name: name, cohort: cohort, hobby: hobby, cob: cob})
     end
       
-    students.push({name: name, cohort: :november, hobby: hobby, cob: cob})
+    students.push({name: name, cohort: cohort, hobby: hobby, cob: cob})
     puts "Now we have #{students.length} students."
     puts "Please enter the name of a student you want to add:"
     name = gets.chomp
@@ -76,5 +78,5 @@ end
 # Nothing happens until we call the methods
 data = input_students
 print_header
-print(data[0], data[1], data[2])
+print_students(data[0], data[1], data[2])
 print_footer(data[0], data[1], data[2])
